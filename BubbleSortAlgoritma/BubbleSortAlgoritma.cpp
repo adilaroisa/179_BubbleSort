703399f9ffa19b8b120b4cede87b9916053b61aa
adilaroisa <adilaroisa688@gmail.com>
2024-03-07 12:02:01 +0700

    Membuat prosedur untuk mengurutkan array dengan metode buuble sort

--- BubbleSortAlgoritma/BubbleSortAlgoritma.cpp
+++ BubbleSortAlgoritma/BubbleSortAlgoritma.cpp
@@ -37,6 +37,20 @@ void display() { // Procedure menampilkan hasil
     }
     cout << endl; // Outputt baris kosong
 }
+
+void bubbleSortArray() { //Procedure untuk mengurutkan array dengan metode buuble sort
+    int pass = 1; // Step 1
+    for (pass;pass <= n-1; pass + 1){ // Looping dengan j dimulai dari 1 hingga n-1
+        for (int j = 0; j <= n - 1 - pass; j++) { // Looping dengan j dimulai dari 0 hingga n-1
+            if (a[j] > a[j + 1]) { // Jika nilai pada a[j] lebih besar dari a[j+1]
+                int temp = a[j]; // Simpan nilai a[j] ke variabel sementara temp
+                a[j] = a[j + 1]; // Assign nilai a[j+1] ke a[j]
+                a[j + 1] = temp; // Assign nilai temp ke a[j+1]
+            }          
+    }
+        }
+
+}
 int main()
 {
     
